ee621a978a8148ea2e27bdd63e7a7ad263e43c5e
DELL <DELL@DESKTOP-IBHOD3E>
2023-03-04 11:19:56 +0300

    Add project files.

---
+++ BarcodeExample/Classes/Item.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarcodeExample.Classes
+{
+    internal class Item
+    {
+        #region Variables
+        public int CategoryNumber { get; set; }
+        public decimal ItemPrice { get; set; }
+        public int ItemNumber { get; set; }
+        public string? ItemName { get; set; }
+        #endregion
+
+        #region Methodes
+        public string GetFormatedCategoryNumber(int number)
+        {
+            String NewNumber = Convert.ToString(number);
+            string returnedNumber = string.Empty;
+            for (int i = 0; i < 3 - NewNumber.Length; i++)
+            {
+                returnedNumber += "0";
+            }
+            return returnedNumber + NewNumber;
+        }
+
+        public string GetFormatedPriceNumber(decimal number)
+        {
+            String NewNumber = Convert.ToString(number);
+            string returnedNumber = string.Empty;
+            for (int i = 0; i < 5 - NewNumber.Length; i++)
+            {
+                returnedNumber += "0";
+            }
+            return returnedNumber + NewNumber;
+        }
+
+
+        public string GetFormatedItemNumber(int number)
+        {
+            String NewNumber = Convert.ToString(number);
+            string returnedNumber = string.Empty;
+            for (int i = 0; i < 5 - NewNumber.Length; i++)
+            {
+                returnedNumber += "0";
+            }
+            return returnedNumber + NewNumber;
+        }
+
+        #endregion
+    }
+}
---
+++ BarcodeExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using BarcodeExample.Classes;
+namespace BarcodeExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to barcode app!");
+            // Barcode (item category --- > 3n   // item price     --- > 5n    // item number     --- > 5n)
+            string Barcodes = @"0010020000001-0020050000002-0030040000003-0040150000013";
+            string[] BarcodeArray = Barcodes.Split('-');
+
+            Item OItem = new Item();
+            List<Item> ListOfItems = new List<Item>();
+
+            foreach (string xBarcode in BarcodeArray)
+            {
+                OItem = new Item();
+                OItem.CategoryNumber = Convert.ToInt32(xBarcode.Substring(0, 3));
+                OItem.ItemPrice = Convert.ToDecimal(xBarcode.Substring(3, 5));
+                OItem.ItemNumber = Convert.ToInt32(xBarcode.Substring(8, 5));
+                OItem.ItemName = Console.ReadLine();//from user
+
+                ListOfItems.Add(OItem);
+            }
+
+            string NewBarcodes = string.Empty;
+            foreach (var x in ListOfItems)
+            {
+                NewBarcodes += $"-{OItem.GetFormatedCategoryNumber(x.CategoryNumber)}-{OItem.GetFormatedPriceNumber(x.ItemPrice)}-{OItem.GetFormatedItemNumber(x.ItemNumber)} \n {OItem.ItemName}";
+            }
+
+
+        }
+    }
+}
